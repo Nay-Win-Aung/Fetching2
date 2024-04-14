3c164ff78b8681a24ad9d536d008f71f252fd214
Nay Win Aung <naywinaung2102022@gmail.com>
2024-04-14 20:51:50 +0630

    14.4.2024/8:51pm

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    // Check if the request is for the fetch_info.txt file
+    if (req.url === '/fetch_info.txt') {
+        fs.readFile('fetch_info.txt', (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('File not found');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(data);
+            }
+        });
+    } else {
+        // Serve HTML file for other requests
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('File not found');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
